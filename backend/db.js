9893c4b7b8ccc52fd9c85d450ee7f9a16a899ba6
Christine Macharia <christineolivia7gmail.com>
2025-04-13 18:20:20 -0700

    added testing documentation into my readme and modified db.js in preparation for heroku deployment

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,9 @@ const { Pool } = require('pg');
 const { DATABASE_URL } = require('./config');
 
 const db = new Pool({
-  connectionString: DATABASE_URL
+  connectionString: DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
+
 
 module.exports = db;
